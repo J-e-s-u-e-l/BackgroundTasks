878814031984b9515a0953a9bee0afb560d31350
Ogunfowokan Emmanuel <112766711+J-e-s-u-e-l@users.noreply.github.com>
2024-04-16 11:54:16 +0100

    Add project files.

---
+++ MyBackgroundService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BackgroundTasks
+{
+    public class MyBackgroundService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
